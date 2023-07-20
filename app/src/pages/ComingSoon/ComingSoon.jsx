07732e1eec0b4b74ef7da66c52c65146805cd6ec
Michael Gainey <mgainey@hawaii.edu>
2023-07-19 14:48:10 -1000

    edit

--- app/src/pages/ComingSoon/ComingSoon.jsx
+++ app/src/pages/ComingSoon/ComingSoon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../components/Logo';
+import Logo from '../../components/Logo';
 import Container from 'react-bootstrap/Container';
 // import Button from 'react-bootstrap/Button';
 
